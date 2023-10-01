f4936bbc7ed341ad5c383994c880d9a4e83bed44
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2023-10-01 15:27:08 +0900

    CSVRecorder

---
+++ Assets/DesignDemo/Scripts/TimeRecorders/Impls/CSVRecorder.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class CSVRecorder : MonoBehaviour, ITimeRecordable
+{
+    private string _csvPath = Application.dataPath + "/../record/csv";
+    public event Action OnTimeRecorded;
+    
+    public List<float> GetTimeRanking(int num)
+    {
+        if (!File.Exists(_csvPath))
+            return new List<float>();
+        return File.ReadLines(_csvPath).SkipLast(1).Select(v => float.Parse(v)).ToList();
+    }
+
+    public void RecordTime(float time)
+    {
+        File.AppendAllText(_csvPath, "\n" + time);
+    }
+}
