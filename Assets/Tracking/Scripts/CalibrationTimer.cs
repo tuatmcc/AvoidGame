7b94b761982ff535ff6231c20bf3666ba9780c7f
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-10-12 14:47:30 +0900

    [update]: ienumerator to unitask

--- Assets/Tracking/Scripts/CalibrationTimer.cs
+++ Assets/Tracking/Scripts/CalibrationTimer.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
@@ -7,16 +9,16 @@ namespace Tracking
 {
     public class CalibrationTimer : MonoBehaviour
     {
+        [SerializeField] private Avatar avatar;
         public Server server;
         public int timer = 5;
         public KeyCode calibrationKey = KeyCode.C;
-        public TextMeshProUGUI text;
 
         private bool calibrated;
 
         private void Start()
         {
-            text.text = "Press " + calibrationKey + " to start calibration timer.";
+            Debug.Log("Press " + calibrationKey + " to start calibration timer.");
         }
 
         private void Update()
@@ -26,50 +28,37 @@ namespace Tracking
                 if (!calibrated)
                 {
                     calibrated = true;
-                    StartCoroutine(Timer());
+                    var token = this.GetCancellationTokenOnDestroy();
+                    Timer(token).Forget();
                 }
                 else
                 {
-                    StartCoroutine(Notify());
+                    Notify().Forget();
                 }
             }
         }
 
-        private IEnumerator Timer()
+        private async UniTask Timer(CancellationToken token)
         {
             int t = timer;
-            while (t > 0)
+            while (t > 0 && !token.IsCancellationRequested)
             {
-                text.text = "Copy the avatars starting pose: " + t.ToString();
-                yield return new WaitForSeconds(1f);
+                Debug.Log("Copy the avatars starting pose: " + t.ToString());
+                await UniTask.WaitForSeconds(1f, cancellationToken: token);
                 --t;
             }
 
-            Avatar[] a = FindObjectsByType<Avatar>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
-            foreach (Avatar aa in a)
-            {
-                aa.Calibrate();
-            }
+            avatar.Calibrate();
+            Debug.Log("Calibration Completed");
+            server.SetVisible(false);
 
-            if (a.Length > 0)
-            {
-                text.text = "Calibration Completed";
-                server.SetVisible(false);
-            }
-            else
-            {
-                text.text = "Avatar in scene not found...";
-            }
-
-            yield return new WaitForSeconds(1.5f);
-            text.text = "";
+            await UniTask.WaitForSeconds(1.5f, cancellationToken: token);
         }
 
-        private IEnumerator Notify()
+        private async UniTask Notify()
         {
-            text.text = "Must restart instance to recalibrate."; // currently a limitation of the way things are set up
-            yield return new WaitForSeconds(3f);
-            text.text = "";
+            Debug.Log("Must restart instance to recalibrate.");
+            await UniTask.WaitForSeconds(3f);
         }
     }
 }
