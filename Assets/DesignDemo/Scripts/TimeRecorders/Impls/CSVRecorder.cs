7b3749741a981069e37731602e8672b3c809f536
kitashimauni <kitashima.daiki123@gmail.com>
2023-10-12 11:04:53 +0900

    タイムの保存型を変更

--- Assets/DesignDemo/Scripts/TimeRecorders/Impls/CSVRecorder.cs
+++ Assets/DesignDemo/Scripts/TimeRecorders/Impls/CSVRecorder.cs
@@ -10,14 +10,14 @@ public class CSVRecorder : MonoBehaviour, ITimeRecordable
     private string _csvPath = Application.dataPath + "/../record.csv";
     public event Action OnTimeRecorded;
 
-    public List<float> GetTimeRanking(int num)
+    public List<long> GetTimeRanking()
     {
         if (!File.Exists(_csvPath))
-            return new List<float>();
-        return File.ReadLines(_csvPath).SkipLast(1).Select(v => float.Parse(v)).ToList();
+            return new List<long>();
+        return File.ReadLines(_csvPath).SkipLast(1).Select(v => long.Parse(v)).ToList();
     }
 
-    public void RecordTime(float time)
+    public void RecordTime(long time)
     {
         File.AppendAllText(_csvPath, time + "\n");
     }
