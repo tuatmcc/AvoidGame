ac5dae83f0817bbb3e2eae4ca96b25441648c371
kitashimauni <kitashima.daiki123@gmail.com>
2023-10-18 19:05:19 +0900

    Manager類をDontDestroyOnLoadに

---
+++ Assets/AvoidGame/Scripts/TimeManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+/// <summary>
+/// ゲームの時間を管理する
+/// </summary>
+namespace AvoidGame
+{
+    public class TimeManager : MonoBehaviour
+    {
+        private bool counting = false;
+
+        private long start_time;
+
+        public event Action<TimeSpan> OnTimeChanged;
+
+        public long MainTimer
+        {
+            get => _mainTimer;
+            set
+            {
+                _mainTimer = value;
+                OnTimeChanged?.Invoke(new TimeSpan(MainTimer));
+            }
+        }
+
+        private long _mainTimer;
+
+        [Inject] private GameStateManager _gameStateManager;
+        private ITimeRecordable _timeRecordable;
+
+        public void Start()
+        {
+            _gameStateManager.OnGameStateChanged += ChangeCount;
+            // _timeRecordable = GameObject.Find("Static").GetComponent<ITimeRecordable>();
+        }
+
+        private void ChangeCount(GameState gameState)
+        {
+            switch (gameState)
+            {
+                case GameState.Playing:
+                    StartCount();
+                    break;
+                case GameState.Result:
+                    StopCount(); 
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Update()
+        {
+            if (counting)
+            {
+                MainTimer = DateTime.Now.Ticks - start_time;
+            }
+        }
+
+        public void StartCount()
+        {
+            if(counting) return; 
+            MainTimer = 0;
+            start_time = DateTime.Now.Ticks;
+            counting = true;
+        }
+
+        public void StopCount()
+        {
+            _timeRecordable.RecordTime(MainTimer);
+            counting = false;
+        }
+    }
+}
