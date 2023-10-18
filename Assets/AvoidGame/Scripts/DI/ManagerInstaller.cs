499b112151581dbcf1c7c66c774ebc6ddbf331eb
kitashimauni <kitashima.daiki123@gmail.com>
2023-10-18 20:03:53 +0900

    DIテスト

---
+++ Assets/AvoidGame/Scripts/DI/ManagerInstaller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace AvoidGame.DI
+{
+    public class ManagerInstaller : MonoInstaller
+    {
+        [SerializeField] private GameStateManager _gameStateManager;
+        [SerializeField] TimeManager _timeManager;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<GameStateManager>()
+                .FromInstance(_gameStateManager)
+                .AsSingle();
+
+            Container.Bind<TimeManager>()
+                .FromInstance(_timeManager)
+                .AsSingle();
+        }
+    }
+}
+
