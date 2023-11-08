65231bfecdf7afbd874acfcac2af5be9ef3521c9
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-11-08 20:02:52 +0900

    均等に配置

--- Assets/AvoidGame/Scripts/Play/ItemGenerator.cs
+++ Assets/AvoidGame/Scripts/Play/ItemGenerator.cs
@@ -1,27 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-/// <summary>
-/// パス上にアイテムを生成する
-/// </summary>
 namespace AvoidGame.Play
 {
+    /// <summary>
+    /// パス上にアイテムを生成する
+    /// </summary>
     public class ItemGenerator : MonoBehaviour
     {
-        [SerializeField] Cinemachine.CinemachinePath path;
-        [SerializeField] List<GameObject> objs;
-        [SerializeField, Range(0.0f, 5.0f)] List<float> itemPosition;
+        [SerializeField] CinemachinePath path;
+        [SerializeField] List<CinemachineDollyCart> itemPatterns;
+        [SerializeField] private int itemCount = 15;
+
+
+        private void AlignItemPatterns()
+        {
+            var space = path.PathLength / (itemCount + 1);
+            for (var i = 0; i < itemCount; i++)
+            {
+                var item = Instantiate(itemPatterns[Random.Range(0, itemPatterns.Count - 1)].gameObject);
+                var cart = item.GetComponent<CinemachineDollyCart>();
+                cart.m_Position = space * (i + 1);
+                cart.m_Path = path;
+            }
+        }
 
         void Start()
         {
-            foreach(var position in itemPosition)
-            {
-                var pos = path.EvaluateLocalPosition(position);
-                var rotation = path.EvaluateOrientation(position);
-                var rot = Quaternion.AngleAxis(90.0f, Vector3.up);
-                Instantiate(objs[Random.Range(0, objs.Count)], pos, rot * rotation, this.transform);
-            }
+            AlignItemPatterns();
         }
     }
 }
