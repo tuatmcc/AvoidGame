badff61c41ae67ff9306b9e2ff3ef22998f280d5
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-11-08 18:35:39 +0900

    UniTas完璧や

--- Assets/AvoidGame/Scripts/Calibration/CalibrationStateManager.cs
+++ Assets/AvoidGame/Scripts/Calibration/CalibrationStateManager.cs
@@ -19,7 +19,9 @@ namespace AvoidGame.Calibration
         private CalibrationState _state = CalibrationState.Waiting;
 
         public float CalibratingDuration { get; } = 5f;
-        public float FinishedDuration { get; } = 60f;
+        public float DissolvingDuration { get; } = 5f;
+        public float FinishedDuration { get; } = 30f;
+
 
         public float TransitioningDuration { get; } = 3f;
 
@@ -38,48 +40,92 @@ namespace AvoidGame.Calibration
 
         private CancellationTokenSource _cts;
 
-
         public void Initialize()
         {
             _cts = new CancellationTokenSource();
-            StartTimerAsync(_cts.Token).Forget();
+            StartWaitingAsync(_cts.Token).Forget();
+            OnCalibrationStateChanged += HandleCalibrationStateChange;
         }
 
         private async UniTask StartTimerAsync(CancellationToken token)
         {
             // Wait for MediaPipeManager to be ready.(MediaPipeManager is ready when it receives the first landmark data.)
             State = CalibrationState.Waiting;
-            while (!_mediaPipeManager.IsReady)
+            while ((!_mediaPipeManager.IsReady || State == CalibrationState.Waiting) && !token.IsCancellationRequested)
             {
                 await UniTask.Delay(500, cancellationToken: token);
                 Debug.Log($"Waiting for MediaPipe. IsReady: {_mediaPipeManager.IsReady}");
             }
 
-            // Start calibration.
-            State = CalibrationState.Calibrating;
-            await UniTask.Delay(TimeSpan.FromSeconds(CalibratingDuration), cancellationToken: _cts.Token);
-
-
-            // Finish calibration.
-            // if (State != CalibrationState.Finished)
-            // {
-            State = CalibrationState.Finished;
-            // }
-
-            await UniTask.Delay(TimeSpan.FromSeconds(FinishedDuration), cancellationToken: _cts.Token);
-
-            // if (State != CalibrationState.Transitioning)
-            // {
-            State = CalibrationState.Transitioning;
-            // }
-
-            await UniTask.Delay(TimeSpan.FromSeconds(TransitioningDuration), cancellationToken: _cts.Token);
-            _gameStateManager.GameState = GameState.Play;
+            // Start Calibration
+            if (State != CalibrationState.Calibrating)
+                State = CalibrationState.Calibrating;
         }
 
         public void Dispose()
         {
             _cts?.Cancel();
+        }
+
+        private void HandleCalibrationStateChange(CalibrationState state)
+        {
+            switch (state)
+            {
+                case CalibrationState.Waiting:
+                    break;
+                case CalibrationState.Calibrating:
+                    _cts = new CancellationTokenSource();
+                    StartCalibratingAsync(_cts.Token).Forget();
+                    break;
+                case CalibrationState.Dissolving:
+                    _cts = new CancellationTokenSource();
+                    StartDissolvingAsync(_cts.Token).Forget();
+                    break;
+                case CalibrationState.Finished:
+                    _cts = new CancellationTokenSource();
+                    StartFinishedAsync(_cts.Token).Forget();
+                    break;
+                case CalibrationState.Transitioning:
+                    _cts = new CancellationTokenSource();
+                    StartTransitionAsync(_cts.Token).Forget();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private async UniTask StartWaitingAsync(CancellationToken token)
+        {
+            await UniTask.WaitUntil(() => _mediaPipeManager.IsReady, cancellationToken: token);
+            if (State == CalibrationState.Waiting)
+                State++;
+        }
+
+        private async UniTask StartDissolvingAsync(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(DissolvingDuration), cancellationToken: token);
+            if (State == CalibrationState.Dissolving)
+                State++;
+        }
+
+        private async UniTask StartTransitionAsync(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(TransitioningDuration), cancellationToken: token);
+            _gameStateManager.GameState = GameState.Title;
+        }
+
+        private async UniTask StartCalibratingAsync(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(CalibratingDuration), cancellationToken: token);
+            if (State == CalibrationState.Calibrating)
+                State++;
+        }
+
+        private async UniTask StartFinishedAsync(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(FinishedDuration), cancellationToken: token);
+            if (State == CalibrationState.Finished)
+                State++;
         }
     }
 }
