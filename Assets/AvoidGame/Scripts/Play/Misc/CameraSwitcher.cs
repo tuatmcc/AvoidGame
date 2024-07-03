03c9aacd8d55f04a50031c8c25b2d81e312d8c17
TUAT-MCC <tuatmcc.com@gmail.com>
2024-07-03 19:44:31 +0900

    add camera work

---
+++ Assets/AvoidGame/Scripts/Play/Misc/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using Cinemachine;
+using UnityEngine;
+using Zenject;
+
+namespace AvoidGame.Play.Misc
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera introCamera;
+        [SerializeField] private CinemachineVirtualCamera playerFollowCamera;
+
+        [Inject] private PlaySceneManager _playSceneManager;
+
+        private void Start()
+        {
+            _playSceneManager.OnCountChanged += count =>
+            {
+                if (count == 5)
+                {
+                    introCamera.Priority = 0;
+                    playerFollowCamera.Priority = 20;
+                }
+            };
+        }
+    }
+}
---
+++ Assets/AvoidGame/Scripts/Play/Misc/IntroCameraController.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace AvoidGame.Play.Misc
+{
+    public class IntroCameraController : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera introCamera;
+        [SerializeField] private Transform playerToLookAt;
+        private readonly float speed = 0.5f;
+        private CinemachineTrackedDolly _dolly;
+
+        private void Start()
+        {
+            _dolly = introCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+            _dolly.m_PathPosition = 0;
+            introCamera.m_LookAt = playerToLookAt;
+        }
+
+        private void Update()
+        {
+            _dolly.m_PathPosition += speed * Time.deltaTime;
+        }
+    }
+}
